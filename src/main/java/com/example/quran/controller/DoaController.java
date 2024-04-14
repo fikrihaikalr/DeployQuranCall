d205d20bc0baa24d8c04943150147770c329b84f
turun19 <faturrizqy19@gmail.com>
2024-04-14 22:50:43 +0700

    tambahan

--- src/main/java/com/example/quran/controller/DoaController.java
+++ src/main/java/com/example/quran/controller/DoaController.java
@@ -21,7 +21,7 @@ public class DoaController {
     @Autowired
     DoaService doaService;
 
-    @GetMapping
+    @GetMapping("/list")
     public ResponseEntity<DoaResponse> getListDoa(){
         return ResponseEntity.ok(doaService.getListDoa());
     }
