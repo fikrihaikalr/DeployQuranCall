4cfa572b7d635972adcc5ae216ad600dc9364647
turun19 <faturrizqy19@gmail.com>
2024-04-30 15:13:16 +0700

    perubahan response

--- src/main/java/com/example/quran/controller/MateriController.java
+++ src/main/java/com/example/quran/controller/MateriController.java
@@ -12,6 +12,7 @@ import com.example.quran.services.SubmateriService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -53,7 +54,7 @@ public class MateriController {
         }
     }
 
-    @PostMapping("/create")
+    @PostMapping(value = "/create", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> createSubMateri(@RequestBody @Valid SubMateriDTO subMateriDTO){
         Submateri createdSubmateri = submateriService.createSubMateri(subMateriDTO);
         if(createdSubmateri != null){
