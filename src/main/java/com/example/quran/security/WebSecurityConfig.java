ee5e731a213d5064d1c0e376fb9eafa25a480480
turun19 <faturrizqy19@gmail.com>
2024-05-03 15:47:08 +0700

    perubahan response

--- src/main/java/com/example/quran/security/WebSecurityConfig.java
+++ src/main/java/com/example/quran/security/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder bCryptPasswordEncoder() {
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
