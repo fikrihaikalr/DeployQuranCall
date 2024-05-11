c45fcc8cb09113df9cf829b30d36e3fe1e983c68
turun19 <faturrizqy19@gmail.com>
2024-05-12 01:16:45 +0700

    perubahan response

--- src/main/java/com/example/quran/services/UserService.java
+++ src/main/java/com/example/quran/services/UserService.java
@@ -293,11 +293,11 @@ public class UserService {
 //    }
 
     public void changePassword(Long id, String oldPassword, String newPassword) throws InvalidPasswordException {
+        Users user = usersRepository.findById(id)
+                .orElseThrow(() -> new ExceptionUsername("Bad Request"));
         if (oldPassword == null || newPassword == null) {
             throw new IllegalArgumentException("Old and new passwords cannot be null.");
         }
-        Users user = usersRepository.findById(id)
-                .orElseThrow(() -> new ExceptionUsername("Bad Request"));
 
 
         // Validate old password
